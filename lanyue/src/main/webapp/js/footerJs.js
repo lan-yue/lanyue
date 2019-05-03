0ebbf6d9ca068c56d21a9767182b537aef05d9b9
TanYuanJie <15802832@qq.com>
2019-05-04 00:07:53 +0800

    修改footer字段

--- lanyue/src/main/webapp/js/footerJs.js
+++ lanyue/src/main/webapp/js/footerJs.js
@@ -8,6 +8,6 @@ function footer() {
         for(i=0;i<data.hezuo.length;i++){
             hezuo.append("<li><a href='javascript:void(0);' class='py-2 d-block'>"+data.hezuo[i].cname+"</a></li>")
         }
-        lianxi.html("<li><span class='icon icon-map-marker'></span><span class='text'>"+data.lianxi.address+"</span></li><li><a href='#'><span class='icon icon-phone'></span><span class='text'>"+data.lianxi.telephone+"</span></a></li><li><a href='#'><span class='icon icon-envelope'></span><span class='text'>"+data.lianxi.email+"</span></a></li>")
+        lianxi.html("<li><span class='icon icon-map-marker'></span><span class='text'>"+data.lianxi.address+"</span></li><li><a href='#'><span class='icon icon-phone'></span><span class='text'>"+data.lianxi.tel+"</span></a></li><li><a href='#'><span class='icon icon-envelope'></span><span class='text'>"+data.lianxi.email+"</span></a></li>")
     })
 }
